9bdc7a8f022033dc98d967a38f3e7d6151c41fc6
Phillip Malboeuf <phil@phils.computer>
2021-11-18 09:20:52 -0500

    connexe if date

--- src/lib/components/Connexe.svelte
+++ src/lib/components/Connexe.svelte
@@ -3,7 +3,8 @@
   import Picture from './Picture.svelte'
   import type { Item } from './Collection.svelte'
   import Themes from './Themes.svelte'
-import I from './icons/I.svelte';
+  import I from './icons/I.svelte'
+  import { date } from '$lib/formatters'
 
   export let items: Entry<Item>[]
 
@@ -14,7 +15,7 @@ import I from './icons/I.svelte';
 
 <section class="grid" style="grid-template-columns: repeat({grid}, 1fr);">  
   <h6><I i="arrow-far-right" /></h6>
-  <h5 style="grid-column: span {grid};">Contenu connexe</h5>
+  <h5 style="grid-column: span {grid};">Aller plus loin</h5>
 
   {#each items as item, index}
   <article>
@@ -28,7 +29,7 @@ import I from './icons/I.svelte';
       <Themes base={item.sys.contentType.sys.id} themes={item.fields.themes} />
 
       <a href="/{item.sys.contentType.sys.id}s/{item.fields.id}"><h5>{item.fields.titre}</h5></a>
-      <small>{item.fields.date}</small>
+      {#if item.fields.date}<small>{date(item.fields.date)}</small>{/if}
     </div>
   </article>
   {/each}
