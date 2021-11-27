d95c75e7e58c7f93189577b3dfe6c305fec3da2e
Phillip Malboeuf <phil@phils.computer>
2021-11-27 14:34:32 -0500

    svelte config fix

--- src/lib/clients/contentful.ts
+++ src/lib/clients/contentful.ts
@@ -1,6 +1,4 @@
-// import { createClient } from 'contentful'
-import pkg from 'contentful'
-const { createClient } = pkg
+import { createClient } from 'contentful'
 
 export const contentful = createClient({
   space: '9m5iliq43y44',
