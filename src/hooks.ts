566b23bbfc28afd19be9f00f3e5c07a38e415c01
Phillip Malboeuf <phil@phils.computer>
2021-12-11 12:06:58 -0500

    link header

--- src/hooks.ts
+++ src/hooks.ts
@@ -7,6 +7,7 @@ export async function handle({ request, resolve }) {
 		...response,
 		headers: {
 			...response.headers,
+			'Link': '<https://images.ctfassets.net>; rel="preconnect", <https://fonts.googleapis.com>; rel="preconnect", <https://fonts.gstatic.com>; rel="preconnect"; crossorigin, <https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap>; rel="preload"; as="style"',
 			'Cache-Control': 's-maxage=1, stale-while-revalidate'
 		}
 	};
