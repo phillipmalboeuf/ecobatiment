2e8e6feefec9440e8092b7474297f4c3a08e4de1
Phillip Malboeuf <phil@phils.computer>
2025-08-20 13:09:04 -0400

    cache

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,8 @@ export async function handle({ event, resolve }) {
 
 	const response = await resolve(event)
 	response.headers.set('Link', '<https://images.ctfassets.net>; rel="preconnect", <https://fonts.googleapis.com>; rel="preconnect", <https://fonts.gstatic.com>; rel="preconnect"; crossorigin, <https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap>; rel="preload"; as="style"')
-	response.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate')
+	response.headers.set('Vercel-CDN-Cache-Control', 'maxage=3600000, stale-while-revalidate')
+	response.headers.set('Cache-Control', 'maxage=3600000, stale-while-revalidate')
 
 	return response
 }
--- src/lib/components/Picture.svelte
+++ src/lib/components/Picture.svelte
@@ -1,9 +1,13 @@
-<script>
+<script lang="ts">
   export let media
   export let small = false
   export let noDescription = false
   export let ar = undefined
   export let eager = false
+
+  function cdn(url: string) {
+    return url.replace('//downloads.ctfassets.net', '//images.ctfassets.net').replace('//images.ctfassets.net', '//philsassets.imgix.net')
+  }
 </script>
 
 <style>
@@ -28,18 +32,18 @@
 {:else}
 <picture>
   {#if small}
-<source srcSet="{media.fields.file.url}?w=400{ar ? `&h=${Math.round(ar * 400)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 900px)" />
-<source srcSet="{media.fields.file.url}?w=400{ar ? `&h=${Math.round(ar * 400)}&fit=fill` : ''}" media="(max-width: 900px)" />
-<source srcSet="{media.fields.file.url}?w=600{ar ? `&h=${Math.round(ar * 600)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 1200px)" />
-<source srcSet="{media.fields.file.url}?w=600{ar ? `&h=${Math.round(ar * 600)}&fit=fill` : ''}" media="(max-width: 1200px)" />
-<source srcSet="{media.fields.file.url}?w=800{ar ? `&h=${Math.round(ar * 800)}&fit=fill` : ''}&fm=avif" type="image/avif" />
-<img src="{media.fields.file.url}?w=800{ar ? `&h=${Math.round(ar * 800)}&fit=fill` : ''}" alt="{media.fields.title} {media.fields.description}" loading={eager ? "eager" : "lazy"} />
+<source srcSet="{cdn(media.fields.file.url)}?w=400{ar ? `&h=${Math.round(ar * 400)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 900px)" />
+<source srcSet="{cdn(media.fields.file.url)}?w=400{ar ? `&h=${Math.round(ar * 400)}&fit=fill` : ''}" media="(max-width: 900px)" />
+<source srcSet="{cdn(media.fields.file.url)}?w=600{ar ? `&h=${Math.round(ar * 600)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 1200px)" />
+<source srcSet="{cdn(media.fields.file.url)}?w=600{ar ? `&h=${Math.round(ar * 600)}&fit=fill` : ''}" media="(max-width: 1200px)" />
+<source srcSet="{cdn(media.fields.file.url)}?w=800{ar ? `&h=${Math.round(ar * 800)}&fit=fill` : ''}&fm=avif" type="image/avif" />
+<img src="{cdn(media.fields.file.url)}?w=800{ar ? `&h=${Math.round(ar * 800)}&fit=fill` : ''}" alt="{media.fields.title} {media.fields.description}" loading={eager ? "eager" : "lazy"} />
   {:else}
-<source srcSet="{media.fields.file.url}?w=900{ar ? `&h=${Math.round(ar * 900)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 900px)" />
-<source srcSet="{media.fields.file.url}?w=900{ar ? `&h=${Math.round(ar * 900)}&fit=fill` : ''}" media="(max-width: 900px)" />
-<source srcSet="{media.fields.file.url}?w=1200{ar ? `&h=${Math.round(ar * 1200)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 1200px)" />
-<source srcSet="{media.fields.file.url}?w=1800{ar ? `&h=${Math.round(ar * 1800)}&fit=fill` : ''}&fm=avif" type="image/avif" />
-<img style={ar && `aspect-ratio: 1800 / ${Math.round(ar * 1800)}`} src="{media.fields.file.url}?w=1800{ar ? `&h=${Math.round(ar * 1800)}&fit=fill` : ''}" alt="{media.fields.title} {media.fields.description}" loading={eager ? "eager" : "lazy"} />
+<source srcSet="{cdn(media.fields.file.url)}?w=900{ar ? `&h=${Math.round(ar * 900)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 900px)" />
+<source srcSet="{cdn(media.fields.file.url)}?w=900{ar ? `&h=${Math.round(ar * 900)}&fit=fill` : ''}" media="(max-width: 900px)" />
+<source srcSet="{cdn(media.fields.file.url)}?w=1200{ar ? `&h=${Math.round(ar * 1200)}&fit=fill` : ''}&fm=avif" type="image/avif" media="(max-width: 1200px)" />
+<source srcSet="{cdn(media.fields.file.url)}?w=1800{ar ? `&h=${Math.round(ar * 1800)}&fit=fill` : ''}&fm=avif" type="image/avif" />
+<img style={ar && `aspect-ratio: 1800 / ${Math.round(ar * 1800)}`} src="{cdn(media.fields.file.url)}?w=1800{ar ? `&h=${Math.round(ar * 1800)}&fit=fill` : ''}" alt="{media.fields.title} {media.fields.description}" loading={eager ? "eager" : "lazy"} />
   {/if}
 </picture>
 {/if}
