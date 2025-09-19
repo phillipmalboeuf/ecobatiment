b026b12afbbc76ca1f8b8686e6f5f34b6fe4cbe0
Phillip Malboeuf <phil@phils.computer>
2025-09-19 12:53:02 -0400

    ETag

--- src/hooks.ts
+++ src/hooks.ts
@@ -5,6 +5,7 @@ export async function handle({ event, resolve }) {
 	response.headers.set('Link', '<https://images.ctfassets.net>; rel="preconnect", <https://fonts.googleapis.com>; rel="preconnect", <https://fonts.gstatic.com>; rel="preconnect"; crossorigin, <https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap>; rel="preload"; as="style"')
 	response.headers.set('Vercel-CDN-Cache-Control', 'maxage=3600000, stale-while-revalidate')
 	response.headers.set('Cache-Control', 'maxage=3600000, stale-while-revalidate')
+	response.headers.set('ETag', `"${process.env.VERCEL_URL || 'dev'}"`)
 
 	return response
 }
