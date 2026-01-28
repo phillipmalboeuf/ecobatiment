b9536af4cbadc9d884d533f4ac0fbbebc7a012d0
Phillip Malboeuf <phil@phils.computer>
2026-01-28 15:16:55 -0500

    vercel tag

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,9 +3,8 @@ export async function handle({ event, resolve }) {
 
 	const response = await resolve(event)
 	response.headers.set('Link', '<https://images.ctfassets.net>; rel="preconnect", <https://fonts.googleapis.com>; rel="preconnect", <https://fonts.gstatic.com>; rel="preconnect"; crossorigin, <https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap>; rel="preload"; as="style"')
-	response.headers.set('Vercel-CDN-Cache-Control', 'maxage=3600000')
-	response.headers.set('Cache-Control', 'maxage=3600000')
-	response.headers.set('ETag', `"${process.env.VERCEL_URL || 'dev'}"`)
+	response.headers.set('Vercel-CDN-Cache-Control', 'max-age=3600000');
+  response.headers.set('Vercel-Cache-Tag', process.env.VERCEL_URL || 'dev');
 
 	return response
 }
